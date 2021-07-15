98f8cc48938742bb624bed8acfd742cc746c9601
OnikuSpedu <srakibnyc1@gmail.com>
2021-07-15 17:57:37 -0400

    Created add function to modify a local trie

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,79 @@
-#! /usr/bin/env node
-console.log("Hello World!");
+#!/usr/bin/env node
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+
+const fs = require('fs');
+
+const fileName = "./trie.json"
+let rawdata = fs.readFileSync(fileName);
+let trie = JSON.parse(rawdata);
+
+function add(str, map) {
+    if (str.length == 0) {
+        map["self"] = true;
+        return map;
+    } else {
+        console.log(map)
+        if(map[str.substring(0,1)]) {
+            console.log("here")
+            map[str.substring(0,1)] = add(str.substring(1), map[str.substring(0,1)]);
+            return map;
+        } else {
+            map[str.substring(0,1)] = add(str.substring(1), {});
+            return map;
+        }
+    }
+}
+
+
+
+
+const argv = yargs(hideBin(process.argv)).options({
+    'd': {
+      alias: 'del',
+      describe: 'Delete keyword from trie',
+      type: 'string'
+    },
+    'a': {
+      alias: 'add',
+      describe: 'Add keyword from trie',
+      type: 'string'
+    },
+    's': {
+        alias: 'search',
+        describe: 'Search keyword',
+        type: 'string'
+      },
+    'f': {
+        alias: 'autocomplete',
+        describe: 'Suggest keyword(s) trie',
+        type: 'string'
+      },
+    'p': {
+        alias: 'display',
+        describe: 'Suggest keyword(s) trie',
+        type: 'string'
+      },
+  }).argv
+
+if (argv["del"]) {
+    console.log(`Deleted ${argv["del"]}`)
+} else if (argv["add"]) {
+    console.log(trie)
+    mod = add(argv["add"], trie);
+    console.log(mod);
+
+    fs.writeFile(fileName, JSON.stringify(mod), function writeJSON(err) {
+        if (err) return console.log(err);
+    });
+    console.log(`Added ${argv["add"]}`)
+} else if (argv["search"]) {
+    console.log('Retreat from the xupptumblers!')
+} else if (argv["autocomplete"]) {
+    console.log('Retreat from the xupptumblers!')
+} else if (argv["display"]) {
+    console.log('Retreat from the xupptumblers!')
+} else {
+    console.log("Aaargs")
+}
